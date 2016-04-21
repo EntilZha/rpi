1e38e7295f85861cc6471713b49e6b483664a422
Pedro Rodriguez <ski.rodriguez@gmail.com>
2016-04-21 16:03:19 -0600

    MDT

--- temperature/management/commands/record_temperature.py
+++ temperature/management/commands/record_temperature.py
@@ -11,7 +11,7 @@ class Command(BaseCommand):
             ['pcsensor'], stdout=subprocess.PIPE).stdout.decode('utf-8')
         tokens = raw_temp.strip().split()
         zone = get_current_timezone()
-        datetime_str = '{0} {1} MST'.format(tokens[0], tokens[1])
+        datetime_str = '{0} {1} MDT'.format(tokens[0], tokens[1])
         recorded_time = datetime.strptime(datetime_str, '%Y/%m/%d %H:%M:%S %Z')
         temp_f = float(tokens[3].replace('F', ''))
         temp_c = float(tokens[4].replace('C', ''))
