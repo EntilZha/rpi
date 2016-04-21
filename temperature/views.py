0bc9da48f57e219360327faf4551ead5dfe4d4cf
Pedro Rodriguez <ski.rodriguez@gmail.com>
2016-04-21 13:58:46 -0600

    added bootstrap and dashboard page

--- temperature/views.py
+++ temperature/views.py
@@ -1,8 +1,8 @@
-from django.http import HttpResponse
+from django.shortcuts import render
 from temperature.models import TemperatureReading
 
 
 def index(request):
-    temps = TemperatureReading.objects.order_by('-recorded_time')
-    output = '\n'.join([str((t.recorded_time, t.temperature_f, t.temperature_c)) for t in temps])
-    return HttpResponse(output)
+    temperature_list = TemperatureReading.objects.order_by('-recorded_time')
+    context = {'temperature_list': temperature_list}
+    return render(request, 'temperature/index.html', context)
