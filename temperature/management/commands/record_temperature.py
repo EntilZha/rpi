5d7fe80f073a400fbc36126b90090f926f076b95
Pedro Rodriguez <ski.rodriguez@gmail.com>
2016-04-21 08:48:09 -0600

    Added basic temp view

---
+++ temperature/management/commands/record_temperature.py
@@ -0,0 +1,23 @@
+import subprocess
+from datetime import datetime
+from django.utils.timezone import get_current_timezone
+from django.core.management.base import BaseCommand
+from temperature.models import TemperatureReading
+
+
+class Command(BaseCommand):
+    def handle(self, *args, **options):
+        raw_temp = subprocess.run(
+            ['pcsensor'], stdout=subprocess.PIPE).stdout.decode('utf-8')
+        tokens = raw_temp.strip().split()
+        zone = get_current_timezone()
+        datetime_str = '{0} {1}'.format(tokens[0], tokens[1])
+        recorded_time = zone.localize(datetime.strptime(datetime_str, '%Y/%m/%d %I:%M:%S'))
+        temp_f = float(tokens[3].replace('F', ''))
+        temp_c = float(tokens[4].replace('C', ''))
+        reading = TemperatureReading(
+            recorded_time=recorded_time, temperature_f=temp_f, temperature_c=temp_c)
+        reading.save()
+        with open('temp.log', 'a') as f:
+            f.write(raw_temp)
+        self.stdout.write(self.style.SUCCESS('Successfully recorded temperature'))
--- temperature/views.py
+++ temperature/views.py
@@ -1,5 +1,8 @@
 from django.http import HttpResponse
+from temperature.models import TemperatureReading
 
 
 def index(request):
-    return HttpResponse('hello world')
+    temps = TemperatureReading.objects.order_by('-recorded_time')
+    output = '\n'.join([str((t.recorded_time, t.temperature_f, t.temperature_c)) for t in temps])
+    return HttpResponse(output)
