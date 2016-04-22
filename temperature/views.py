7655379d8c3cfc28d4c66a2e6060165bd6058618
Pedro Rodriguez <ski.rodriguez@gmail.com>
2016-04-22 00:22:42 -0600

    Added current temp and cleaned things up more

--- temperature/views.py
+++ temperature/views.py
@@ -4,5 +4,5 @@ from temperature.models import TemperatureReading
 
 def index(request):
     temperature_list = TemperatureReading.objects.order_by('-recorded_time')
-    context = {'temperature_list': temperature_list}
+    context = {'temperature_list': temperature_list, 'current_temp': temperature_list[0]}
     return render(request, 'temperature/index.html', context)
