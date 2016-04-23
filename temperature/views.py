5f40c4d0a7f251fab3b5ca7cec2924b3bf658b58
Pedro Rodriguez <ski.rodriguez@gmail.com>
2016-04-22 20:24:34 -0600

    Add a limit

--- temperature/views.py
+++ temperature/views.py
@@ -3,6 +3,6 @@ from temperature.models import TemperatureReading
 
 
 def index(request):
-    temperature_list = TemperatureReading.objects.order_by('-id')
+    temperature_list = TemperatureReading.objects.order_by('-id')[:2880]
     context = {'temperature_list': temperature_list, 'current_temp': temperature_list[0]}
     return render(request, 'temperature/index.html', context)
