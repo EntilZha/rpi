cc0a82f932d9a8c5bc43f404a78060ffaf941b26
Pedro Rodriguez <ski.rodriguez@gmail.com>
2016-04-22 19:58:13 -0600

    More efficient order by

--- temperature/views.py
+++ temperature/views.py
@@ -3,6 +3,6 @@ from temperature.models import TemperatureReading
 
 
 def index(request):
-    temperature_list = TemperatureReading.objects.order_by('-recorded_time')
+    temperature_list = TemperatureReading.objects.order_by('-id')
     context = {'temperature_list': temperature_list, 'current_temp': temperature_list[0]}
     return render(request, 'temperature/index.html', context)
